b82ec859d415a5bc93674f055c5fb1e5eeafc78e
firozrahisuddinsaifi <firozkhandelhi62@gmail.com>
2024-09-14 19:18:48 +0530

    About Component Done

---
+++ src/Components/About.jsx
@@ -0,0 +1,46 @@
+import { AboutContent, AboutContentContainer, AboutEducation, AboutHeading, AboutTitle, AboutTitleContainer, Container, EducationContent, EducationHeading } from "./About.styles"
+import ConstantValues from "../ConstantValues/ConstValue";
+
+
+const {
+    about,
+    whyMe,
+    introContent,
+    education,
+    mDegree,
+    mUniversity,
+    mseason,
+    bDegree,
+    bUniversity,
+    bseason,
+}=ConstantValues
+export const About=()=>{
+    return(<>
+
+            <div>
+                <Container>
+                    <AboutTitleContainer>
+                        <AboutTitle>{about}</AboutTitle> 
+                     <AboutHeading>{whyMe}</AboutHeading>
+                    </AboutTitleContainer>
+                    <AboutContentContainer>
+                        <AboutContent>
+                        {introContent}
+                        </AboutContent>
+                    </AboutContentContainer>
+
+                    <AboutEducation>
+                    <EducationHeading>{education}</EducationHeading>
+                    <EducationHeading>{mDegree}</EducationHeading>
+                    <EducationContent>{mUniversity}</EducationContent>
+                    <EducationContent>{mseason}</EducationContent>
+
+                    <EducationHeading>{bDegree}</EducationHeading>
+                    <EducationContent>{bUniversity}</EducationContent>
+                    <EducationContent>{bseason}</EducationContent>
+                    </AboutEducation>
+                </Container>
+            </div>
+
+    </>)
+}
