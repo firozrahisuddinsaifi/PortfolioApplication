3417f67c19d39d9bf65488ccc8d0ebb7169cc6ed
firozrahisuddinsaifi <firozkhandelhi62@gmail.com>
2024-09-14 19:55:52 +0530

    Contact Component Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { About } from "./Components/About"
+import { Contact } from "./Components/Contact"
 import { Header } from "./Components/HeaderCom"
 import { Projects } from "./Components/Projects"
 import { TechStack } from "./Components/TechStack"
@@ -14,6 +15,7 @@ export const  App=()=> {
     <About />
     <TechStack />
     <Projects />
+    <Contact />
     </>
   )
 }
